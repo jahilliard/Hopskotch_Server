5dce317683f515cc401cbc39ded33dd03ce290db
jahilliard <justin.a.hilliard@gmail.com>
2016-02-17 23:32:03 -0500

    got rid of admin

--- routes/index.js
+++ routes/index.js
@@ -23,36 +23,36 @@ router.get('/login/facebook', AuthController.fbAuthenticate);
 /*
  * Routes that can be accessed only by authenticated & authorized users
  */
-router.get('/api/v1/admin/users/', UserController.getAll);
-router.get('/api/v1/admin/users/:id', UserController.getById);
-router.post('/api/v1/admin/users/:id', UserController.update);
-router.delete('/api/v1/admin/users/:id', UserController.delete);
+router.get('/api/v1/users/', UserController.getAll);
+router.get('/api/v1/users/:id', UserController.getById);
+router.post('/api/v1/users/:id', UserController.update);
+router.delete('/api/v1/users/:id', UserController.delete);
 
-router.get('/api/v1/admin/locations/', LocationController.getAll);
-router.get('/api/v1/admin/locations/:id', LocationController.getById);
-router.post('/api/v1/admin/locations/', LocationController.create);
-router.post('/api/v1/admin/locations/:id', LocationController.update);
-router.delete('/api/v1/admin/locations/:id', LocationController.delete);
+router.get('/api/v1/locations/', LocationController.getAll);
+router.get('/api/v1/locations/:id', LocationController.getById);
+router.post('/api/v1/locations/', LocationController.create);
+router.post('/api/v1/locations/:id', LocationController.update);
+router.delete('/api/v1/locations/:id', LocationController.delete);
 
-router.post('/api/v1/admin/locations/:id/menu/', LocationController.addMenuItems);
-router.delete('/api/v1/admin/locations/:id/menu/', LocationController.deleteMenuItems);
+router.post('/api/v1/locations/:id/menu/', LocationController.addMenuItems);
+router.delete('/api/v1/locations/:id/menu/', LocationController.deleteMenuItems);
 
-router.get('/api/v1/admin/matches/', MatchController.getAll);
-router.get('/api/v1/admin/matches/:id', MatchController.getById);
-router.post('/api/v1/admin/matches/', MatchController.create);
-router.post('/api/v1/admin/matches/:id', MatchController.update);
-router.delete('/api/v1/admin/matches/:id', MatchController.delete);
+router.get('/api/v1/matches/', MatchController.getAll);
+router.get('/api/v1/matches/:id', MatchController.getById);
+router.post('/api/v1/matches/', MatchController.create);
+router.post('/api/v1/matches/:id', MatchController.update);
+router.delete('/api/v1/matches/:id', MatchController.delete);
 
-router.post('/api/v1/admin/matches/:id/matchList/', MatchController.addMatches);
-router.delete('/api/v1/admin/matches/:id/matchList/', MatchController.removeMatches);
+router.post('/api/v1/matches/:id/matchList/', MatchController.addMatches);
+router.delete('/api/v1/matches/:id/matchList/', MatchController.removeMatches);
 
-router.get('/api/v1/admin/rooms/', RoomController.getAll);
-router.get('/api/v1/admin/rooms/:id', RoomController.getById);
-router.post('/api/v1/admin/rooms/', RoomController.create);
-router.post('/api/v1/admin/rooms/:id', RoomController.update);
-router.delete('/api/v1/admin/rooms/:id', RoomController.delete);
+router.get('/api/v1/rooms/', RoomController.getAll);
+router.get('/api/v1/rooms/:id', RoomController.getById);
+router.post('/api/v1/rooms/', RoomController.create);
+router.post('/api/v1/rooms/:id', RoomController.update);
+router.delete('/api/v1/rooms/:id', RoomController.delete);
 
-router.post('/api/v1/admin/rooms/:id/members/', RoomController.addMembersToRoom);
-router.delete('/api/v1/admin/rooms/:id/members/', RoomController.removeMembersFromRoom);
+router.post('/api/v1/rooms/:id/members/', RoomController.addMembersToRoom);
+router.delete('/api/v1/rooms/:id/members/', RoomController.removeMembersFromRoom);
 
 module.exports = router;
